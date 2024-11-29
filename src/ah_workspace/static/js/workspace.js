c17538668286e567247568a4f9a1a7822f894650
Jason Livesay <runvnc@gmail.com>
2024-11-29 02:45:40 -0600

    update

--- src/ah_workspace/static/js/workspace.js
+++ src/ah_workspace/static/js/workspace.js
@@ -3,7 +3,11 @@ window.registerCommandHandler('update_workspace', (data) => {
   console.log('update_workspace', data);
   const workspace = document.querySelector('workspace-area');
   if (workspace) {
-    workspace.setContent(data.args.content);
+    if (data.event == 'partial') {
+      workspace.setContent(data.params.content);
+    } else {
+      workspace.setContent(data.args.content);
+    }
   } else {
     console.error('Workspace area not found');
   }
